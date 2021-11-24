3583a03f5b4c2768bbd9cd4dd948bf58df13c782
honghong <90856538+H-H-C@users.noreply.github.com>
2021-11-24 10:09:51 +0900

    ファイルオープン

---
+++ ex13/ex13_01.php
@@ -0,0 +1,52 @@
+<?php
+$errmsg = [];   //エラーメッセージ
+$filename = 'data/ex13_01.txt'; //ファイル名
+$data = '';   //読み込みファイル
+$msg = '';    //処理結果
+
+
+//ファイル：オープン（読み込みrモード）
+if (($fp = fopen($filename, 'r')) === FALSE)
+{
+  $errmsg[] = $filename . 'モード：rオープンエラー';  //読み込み権限がなかったら
+}
+else
+{
+  while (!feof($fp))
+  {
+    //ファイル：行読み込み
+    $data = rtrim(fgets($fp));
+    $msg .= htmlspecialchars($data, ENT_QUOTES) . '<br />';
+  }
+  //ファイルクローズ
+  fclose($fp);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ex13_01</title>
+</head>
+
+<body>
+  <h1>ファイル読み込み</h1>
+  <?php
+  foreach ($errmsg as $val)
+  {
+    echo $val . '<br />';
+  }
+  if (count($errmsg))
+  {
+    echo "<br />";
+  }
+  ?>
+
+  <?= $msg ?>
+</body>
+
+</html>
